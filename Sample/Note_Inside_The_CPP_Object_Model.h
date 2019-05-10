6e210548132c14d223cf1b85f1d85e5352fff077
415858849 <415858849@qq.com>
2019-05-10 13:08:33 +0800

    modified

--- Sample/Note_Inside_The_CPP_Object_Model.h
+++ Sample/Note_Inside_The_CPP_Object_Model.h
@@ -2,6 +2,16 @@
 #define Note_Inside_The_Cpp_Object_Model
 #ifdef Note_Inside_The_Cpp_Object_Model
 
+/* * C++ 对象模型
+	 *
+	 *
+*/
+
+/* 第一章 关于对象
+	@1.1 The C++ Object Model
+		* 
+
+*/
 
 
 
--- Sample/td.cpp
+++ Sample/td.cpp
@@ -1,4 +1,27 @@
-#include <iostream>
-void show() {
-	std::cout << "show" << std::endl;
+//#include <iostream>
+//void show() {
+//	std::cout << "show" << std::endl;
+//}
+
+
+#include <stdio.h>
+
+
+extern int a;
+//static  int a=4; 
+
+void foo() {
+	//printf("%x\n", &a);
+}
+int main() {
+	extern int a;
+	printf("%x\n", &a);
+	 int a = 10;
+	{
+		//static int a = 10;
+		printf("%x\n", &a);
+	}
+	printf("%x\n", &a);
+	foo();
+	return 0;
 }
